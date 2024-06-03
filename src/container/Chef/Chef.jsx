f3a1dd861ba46fc0a11021b6ceb3d6af0b59074e
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-06-03 23:58:22 +0530

    In This Commit : feat: Add chef photo, signature, and about section to the Chef component
    - Included chef photo and signature images.
    - Added the about section for the chef with quote and description.
    - Styled the Chef component to match the design from the Figma prototype.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
-
+import "./Chef.css";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef-img?" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+          </p>
+        </div>
+        <p className="p__opensans" > ipsum dolor sit amet consectetur adipisicing elit. Temporibus autem odit magnam voluptates aliquam aspernatur, sint architecto incidunt nostrum illo quibusdam aperiam facilis, animi cum magni non facere quidem iusto!</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans"> Chef and Founder</p>
+        <img src={images.sign} alt="" />
+      </div>
+    </div>
   </div>
 );
 
